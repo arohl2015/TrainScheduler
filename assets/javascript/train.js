0d5d5494984a3b0680ac0d886a1e7f1c47e24432
A Rohl <april.rohl2015@gmail.com>
2020-03-14 08:40:10 -0700

    added in firebase key and updated script to html

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBO90jY6oosYEwHSAkHlCqIh3JftA3okNA",
+    authDomain: "train-scheduler-1e628.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1e628.firebaseio.com",
+    projectId: "train-scheduler-1e628",
+    storageBucket: "train-scheduler-1e628.appspot.com",
+    messagingSenderId: "249550498809",
+    appId: "1:249550498809:web:13325855828c784d842b28"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
